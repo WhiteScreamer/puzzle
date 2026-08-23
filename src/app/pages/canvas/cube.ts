7a3212cb7785920d2ba33681186c4d0375d9dcc4
WhiteScreamer <funt2001@ukr.net>
2026-08-24 00:14:54 +0300

    finish correct cube

--- src/app/pages/canvas/cube.ts
+++ src/app/pages/canvas/cube.ts
@@ -6,34 +6,40 @@ import { PositionProvider } from './position-provider';
 export enum Colors {
     red = 0xff0000,
     green = 0x00ff00,
-    orange = 0xffcc00,
+    orange = 0xff9900,
     blue = 0x0000ff,
     white = 0xffffff,
-    нуддщц = 0xffff00,
+    yellow = 0xffff00,
 }
 
 export class Cube implements InteractObject {
-    private geometry = new THREE.BoxGeometry(0.75, 0.75, 0.25);
-    private material = [
-        new THREE.MeshStandardMaterial({ color: Colors.green }),
-        new THREE.MeshStandardMaterial({ color: Colors.orange })
-    ];
+    private posProv = new PositionProvider();
+    private geometry = new THREE.BoxGeometry(0.5, 0.5, 0.25);
     meshes: THREE.Mesh[] = [];
     group!: THREE.Group;
-    private createMesh(scene:THREE.Scene,materialIndex:number, axis:THREE.Vector3,angle:number){
-        const mesh = new THREE.Mesh(this.geometry.clone(), this.material[materialIndex]);
-        mesh.position.add(new THREE.Vector3(0, 0, 0.5 + 0.25 / 2));
-        const posProv = new PositionProvider();
-        posProv.rotate(scene,mesh,new THREE.Vector3(0,0,0),axis,angle);
+    private createMesh(scene:THREE.Scene,color:number, axis:THREE.Vector3,angle:number){
+        const material=new THREE.MeshStandardMaterial({ color: color });
+        const mesh = new THREE.Mesh(this.geometry.clone(), material);
+        mesh.position.add(new THREE.Vector3(0, 0, 0.5 - 0.25/2));
+        this.posProv.rotate(scene,mesh,new THREE.Vector3(0,0,0),axis,angle);
         this.meshes.push(mesh);
     }
     constructor(scene: THREE.Scene) {
         this.group = new THREE.Group();
         const yA=new THREE.Vector3(0,1,0);
-        //front green
-        this.createMesh(scene,0,yA,0);
-        //right orange
-        this.createMesh(scene,1,yA,Math.PI/2);
+        //front
+        this.createMesh(scene,Colors.green,yA,0);
+        //right
+        this.createMesh(scene,Colors.orange,yA,Math.PI/2);
+        //back
+        this.createMesh(scene,Colors.blue,yA,Math.PI);
+        //left
+        this.createMesh(scene,Colors.red,yA,-Math.PI/2);
+        //up
+        const xA=new THREE.Vector3(1,0,0);
+        this.createMesh(scene,Colors.white,xA,-Math.PI/2);
+        //down
+        this.createMesh(scene,Colors.yellow,xA,Math.PI/2);
         for(const mesh of this.meshes){
             this.group.add(mesh);
         }
